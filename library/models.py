81e1677a1a4feef252225d4dfd6d978967c159cf
arhoont <arhoont@gmail.com>
2013-04-14 22:00:41 +0400

    1-st commit

---
+++ library/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+
+# People
+class Domain(models.Model):
+    domain = models.CharField(max_length=255, primary_key=True)
+
+
+class Person(models.Model):
+    domain = models.ForeignKey(Domain)
+    login = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    pwd = models.CharField(max_length=255)
+    salt = models.CharField(max_length=255)
+    adm = models.IntegerField(default=0)
+    status = models.IntegerField(default=0)
+    session = models.CharField(max_length=255, null=True)
+
+# no!
+# class Email(models.Model):
+#     person = models.ForeignKey(Person)
+#     email = models.CharField(max_length=255, primary_key=True)
+
+
+#Book
+class Author(models.Model):
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    info = models.TextField(null=True)
+
+    class Meta:
+        unique_together = ("fname", "lname")
+        index_together = [["fname", "lname"], ]
+
+
+class Keyword(models.Model):
+    word = models.CharField(max_length=255, primary_key=True)
+    description = models.TextField(null=True)
+
+
+class Language(models.Model):
+    language = models.CharField(max_length=255, primary_key=True)
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=255, primary_key=True)
+    ozon = models.CharField(max_length=255, null=True)
+    title = models.CharField(max_length=255)
+    language = models.ForeignKey(Language)
+    description = models.TextField()
+    authors = models.ManyToManyField(Author)
+    keywords = models.ManyToManyField(Keyword)
+    # image = models.ImageField
+
+class BookItem(models.Model):
+    isbn=models.ForeignKey(Book)
+    owner=models.ForeignKey(Person, related_name="itemowner")
+    reader=models.ForeignKey(Person, related_name="itemreader")
+    value = models.IntegerField(default=1)
+    rdate = models.DateTimeField(null=True)
+
+class ItemStatus(models.Model):
+    item=models.ForeignKey(BookItem)
+    status = models.IntegerField()
+    date = models.DateTimeField(null=True)
+
+# Opinion and Link
+class Opinion(models.Model):
+    person=models.ForeignKey(Person)
+    isbn=models.ForeignKey(Book)
+    date = models.DateTimeField()
+    rating = models.IntegerField()
+    text = models.TextField()
+
+class Link(models.Model):
+    person=models.ForeignKey(Person)
+    isbn=models.ForeignKey(Book)
+    opinion = models.ForeignKey(Opinion, null=True)
+    url = models.CharField(max_length=255)
+    text = models.TextField()
+
+class Request(models.Model):
+    item=models.ForeignKey(BookItem)
+    personFrom=models.ForeignKey(Person, related_name="requestfrom")
+    personTo=models.ForeignKey(Person, related_name="requestto")
+    dateSend = models.DateTimeField()
+    dateReply = models.DateTimeField(null=True)
+    answer= models.IntegerField(null=True)
+    comment = models.TextField(null=True)
+    isRead = models.IntegerField(null=True)
+
+class SysSetting(models.Model):
+    authType = models.IntegerField()
+    transferCd = models.IntegerField()
